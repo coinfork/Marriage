11d1f2fd5700f15339146628bdfa6a57dee63324
Witek <witekradomski@gmail.com>
2019-04-07 11:05:44 -0700

    Marriage ID and ability to update partner address

--- Marriage.sol
+++ Marriage.sol
@@ -9,11 +9,14 @@ contract Marriage {
     address public partner1;
     string public partner1Name;
     bool public partner1Signed = false;
+    address private partner1UpdateAddress;
 
     address public partner2;
     string public partner2Name;
     bool public partner2Signed = false;
+    address private partner2UpdateAddress;
 
+    string  public marriageContractId;
     uint256 public blockchainWeddingDate;
     uint256 public yearsToLockFunds;
     address payable public divorceBurnAddress;
@@ -21,9 +24,10 @@ contract Marriage {
     bool public married = true;
     
     event Married(address indexed _partner1, address indexed _partner2);
-    event Signed(address indexed _partner);
-    event LifeEvent(address indexed _sender, uint256 _time, string _eventDescription);
-    event Wishes(address indexed _sender, uint256 _time, string _note);
+    event Signed(uint256 indexed _partnerNumber, address indexed _partner, string _partnerName);
+    event UpdatedAddress(uint256 indexed _partnerNumber, address indexed _oldAddress, address indexed _newAddress);
+    event LifeEvent(address indexed _sender, string _eventDescription);
+    event Wishes(address indexed _sender, string _note);
     event Divorced(address indexed _partner1, address indexed _partner2, address _initiatedBy);
 
     uint256 constant DAYS_IN_YEAR = 365;
@@ -33,8 +37,9 @@ contract Marriage {
         _;
     }
 
-    /// @param _partner1 The address of the first partner
-    /// @param _partner2 The address of the second partner
+    /// @param _marriageContractId A human-readable identifier or name for this contract.
+    /// @param _partner1 The address of the first partner.
+    /// @param _partner2 The address of the second partner.
     /// @param _yearsToLockFunds How many years to lock funds for. Note: Years are 365 days, leap years are not
     /// considered here.
     /// @param _divorceBurnAddress If a divorce occurs, Ether will be sent here. I recommend supplying the public
@@ -43,12 +48,14 @@ contract Marriage {
     /// rest will be transferred to the user that initiates the divorce function as a prisoner's dilemma incentive
     /// to execute the function.
     constructor(
+        string memory _marriageContractId,
         address _partner1,
         address _partner2,
         uint256 _yearsToLockFunds,
         address payable _divorceBurnAddress,
         uint256 _percentToBurnOnDivorce
     ) public {
+        marriageContractId = _marriageContractId;
         partner1 = _partner1;
         partner2 = _partner2;
         yearsToLockFunds = _yearsToLockFunds;
@@ -66,13 +73,60 @@ contract Marriage {
         if(msg.sender == partner1) {
             partner1Name = _partnerName;
             partner1Signed = true;
+            emit Signed(1, msg.sender, _partnerName);
         }
         else if(msg.sender == partner2) {
             partner2Name = _partnerName;
             partner2Signed = true;
+            emit Signed(2, msg.sender, _partnerName);
         }
-        
-        emit Signed(msg.sender);
+    }
+    
+    /// @notice To update a partner's Ethereum account address, they must first call this function (Step 1).
+    /// Next, they must call the acceptUpdatePartnerAddress function from the new account address, to finalize.
+    /// @param _partnerNumber Which partner is updating their address (1 or 2)
+    /// @param _newAddress The new address to be updated to
+    function updatePartnerAddress(uint256 _partnerNumber, address _newAddress) public onlyPartner {
+        if(_partnerNumber == 1) {
+            require(msg.sender == partner1, "Must be partner 1");
+            partner1UpdateAddress = _newAddress;
+        }
+        else if(_partnerNumber == 2) {
+            require(msg.sender == partner2, "Must be partner 2");
+            partner2UpdateAddress = _newAddress;
+        }
+        else {
+            revert("_partnerNumber must be 1 or 2");
+        }
+    }
+    
+    /// @notice This will permanently change one partner's address their desired new address (Step 2).
+    /// Run this function using the new account AFTER calling updatePartnerAddress from the original account.
+    /// @param _partnerNumber Which partner is accepting their new address change (1 or 2)
+    function acceptUpdatePartnerAddress(uint256 _partnerNumber) public {
+        if(_partnerNumber == 1) {
+            if(msg.sender == partner1UpdateAddress) {
+                emit UpdatedAddress(_partnerNumber, partner1, partner1UpdateAddress);
+                partner1 = partner1UpdateAddress;
+                partner1UpdateAddress = address(0);
+            }
+            else {
+                revert("Please accept from the new account you had provided in updatePartnerAddress");
+            }
+        }
+        else if(_partnerNumber == 2) {
+            if(msg.sender == partner2UpdateAddress) {
+                emit UpdatedAddress(_partnerNumber, partner2, partner2UpdateAddress);
+                partner2 = partner2UpdateAddress;
+                partner2UpdateAddress = address(0);
+            }
+            else {
+                revert("Please accept from the new account you had provided in updatePartnerAddress");
+            }
+        }
+        else {
+            revert("_partnerNumber must be 1 or 2");
+        }
     }
 
     /// @notice Warning: Executing this function will send a percentage of the Ether balance to the divorceBurnAddress
@@ -106,13 +160,13 @@ contract Marriage {
     /// @notice This function may be called by the couple to log significant life events, such as the birth of a child.
     /// @param _eventDescription A human-readable description string
     function addLifeEvent(string memory _eventDescription) public onlyPartner {
-        emit LifeEvent(msg.sender, now, _eventDescription);
+        emit LifeEvent(msg.sender, _eventDescription);
     }
     
     /// @notice This function may be called by friends or public to wish a happy marriage, happy anniversary, etc.
     /// @param _note A human-readable note. Please include your name so the couple may know who the wishes are from.
     function addWishes(string memory _note) public {
-        emit Wishes(msg.sender, now, _note);
+        emit Wishes(msg.sender, _note);
     }
     
     /// @notice Payable fallback function, allows this contract to receive Ether.
